1cd4f8d1650bc7ac9d25c441319684cc0dfe5f0b
Gaurang Belekar <gb@Gaurangs-MacBook-Air.local>
2025-07-08 18:00:49 +0100

    deploy Commit

--- dashboard.py
+++ dashboard.py
@@ -151,8 +151,11 @@ app.layout = html.Div([
     ])
 ])
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050)) 
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 
 # import pandas as pd
